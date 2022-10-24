3971733d93221fc70629e453e9049ef1cba08f46
ivanleonov <treska1315@gmail.com>
2022-10-24 21:05:32 +0300

    added middleware & refreshNews

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Route } from 'react-router-dom';
 import Nav from './Components/Nav.jsx';
-import NewsList from './Components/NewsList.jsx';
-import routes from './routes';
+import NewsList from './Components/News/NewsList.jsx';
+import StoriePage from './Components/StoriePage/StoriePage.jsx';
+import { routes } from './utils.js';
+import { getNewsIds } from './Store/actions.js';
 
 function App() {
+  const dispatch = useDispatch();
+  const currentId = useSelector((state) => state.comments.currentStorieId);
+  useEffect(() => {
+    dispatch(getNewsIds());
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(getNewsIds());
+    }, 60000);
+    return () => clearTimeout(timer);
+  });
+
   return (
     <div className="d-flex flex-column h-100">
       <Nav />
-      <Switch>
-        <Route path={routes.mainPage()}>
-          <NewsList />
-        </Route>
-      </Switch>
+      <Route exact path={routes.mainPage()}>
+        <NewsList />
+      </Route>
+      <Route path={routes.newsItemPage(currentId)}>
+        <StoriePage />
+      </Route>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,53 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './Store/index.js';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import { isDifferent } from './utils.js';
+
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log('current state', store.getState());
+  console.info('dispatching', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+// const idCheck = (store) => (next) => (action) => {
+//   if (action.type === 'ADD_NEWS_IDS') {
+//     const previousState = store.getState();
+//     const previousNews = previousState.stories.newsIds;
+//     const result = next(action);
+//     const nextState = store.getState();
+//     const nextNews = nextState.stories.newsIds;
+//     const isNewsDifferent = isDifferent(previousNews, nextNews);
+//     console.log(isNewsDifferent);
+//     return isNewsDifferent ? result : null;
+//   }
+//   return next(action);
+// };
+
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__;
+const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk, logger), reduxDevtools && reduxDevtools()),
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </Provider>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
